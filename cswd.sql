8650d1fe73d1d070a269cb12d9b63b2ea6e62f32
Romeo Bardillon <romsky.bardillon@gmail.com>
2022-06-11 20:08:10 +0800

    update database

--- cswd.sql
+++ cswd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2022 at 01:07 PM
+-- Generation Time: Jun 11, 2022 at 04:22 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -24,55 +24,70 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pwd_data`
+--
+
+CREATE TABLE `pwd_data` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `senior_citizen_data`
+--
+
+CREATE TABLE `senior_citizen_data` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `solo_parent_data`
+--
+
+CREATE TABLE `solo_parent_data` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
   `first_name` varchar(128) NOT NULL,
   `middle_name` varchar(128) DEFAULT NULL,
-  `last_name` varchar(128) NOT NULL,
-  `username` varchar(254) NOT NULL,
-  `email` varchar(254) NOT NULL,
-  `password` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `first_name`, `middle_name`, `last_name`, `username`, `email`, `password`) VALUES
-(1, 'Romeo Jr', 'Montealegre', 'Bardillon', 'rmbardillon', 'romsky.bardillon@gmail.com', '$2y$10$YX.S7QY846.hLKk1Spl1nOFilqtuyEu0eD4N2PUXfzbd68hgY4/jS'),
-(2, 'Maria Christine', 'Montealegre', 'Del Rosario', 'mmdelrosario', 'tintin@gmail.com', '$2y$10$ojARYvLgD1HO6ZqdjSvGbuDhcLNSSWRDXcy7tPgEyl/pO85riJcHm');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_data`
---
-
-CREATE TABLE `user_data` (
-  `id` int(11) NOT NULL,
-  `FName` varchar(128) NOT NULL,
-  `MName` varchar(128) DEFAULT NULL,
-  `LName` varchar(128) NOT NULL,
-  `suffix` varchar(128) DEFAULT NULL,
-  `sex` enum('male','female') NOT NULL,
-  `nationality` varchar(128) NOT NULL,
-  `status` enum('single','married','widowed','separated','divorced') NOT NULL,
-  `BType` enum('A','B','AB','O','Unknown') DEFAULT NULL,
-  `dob` date NOT NULL,
-  `region` varchar(128) NOT NULL,
-  `province` varchar(128) NOT NULL,
-  `city` varchar(128) NOT NULL,
-  `barangay` varchar(128) NOT NULL,
-  `street` varchar(128) DEFAULT NULL,
-  `address` varchar(128) NOT NULL
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pwd_data`
+--
+ALTER TABLE `pwd_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `senior_citizen_data`
+--
+ALTER TABLE `senior_citizen_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `solo_parent_data`
+--
+ALTER TABLE `solo_parent_data`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -81,25 +96,31 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_data`
---
-ALTER TABLE `user_data`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `pwd_data`
+--
+ALTER TABLE `pwd_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `senior_citizen_data`
+--
+ALTER TABLE `senior_citizen_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `solo_parent_data`
+--
+ALTER TABLE `solo_parent_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user_data`
---
-ALTER TABLE `user_data`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
