31faa36ff40a72ee6c606c31e8e7cd63516def8f
Romeo Bardillon <rmbardillonjr@iskolarngbayan.pup.edu.ph>
2022-06-24 11:30:42 +0800

    updated database

--- cswd.sql
+++ cswd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2022 at 04:22 AM
+-- Generation Time: Jun 24, 2022 at 05:30 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -28,43 +28,67 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `pwd_data` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `senior_citizen_data`
---
-
-CREATE TABLE `senior_citizen_data` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `solo_parent_data`
---
-
-CREATE TABLE `solo_parent_data` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(128) NOT NULL,
-  `last_name` varchar(128) NOT NULL,
-  `first_name` varchar(128) NOT NULL,
-  `middle_name` varchar(128) DEFAULT NULL,
+  `registration_type` enum('New ID','Renewal ID','Lost ID','Transfer','change information') NOT NULL,
+  `pwd_number` varchar(20) DEFAULT NULL,
+  `date_applied` date NOT NULL,
+  `pwd_last_name` varchar(128) NOT NULL,
+  `pwd_first_name` varchar(128) NOT NULL,
+  `pwd_middle_name` varchar(128) DEFAULT NULL,
+  `pwd_suffix` varchar(20) DEFAULT NULL,
+  `type_of_disability` set('Deaf/Hard of Hearing','Intellectual Disability','Learning Disability','Mental Disability','Physical Disablity (Orthopedic)','Psychosocial Disability','Speech and Language Impairment','Visual Disability','Cancer (RA11215)','Rare Disease (RA10747)') DEFAULT NULL,
+  `medical_condition` varchar(1000) DEFAULT NULL,
+  `cause_of_disability` set('Congenital/Inborn','Acquired') DEFAULT NULL,
+  `congenital_inborn` set('Autism','ADHD','Cerebral Palsy','Down Syndrome') DEFAULT NULL,
+  `acquired` set('Chronic Illness','Cerebral Palsy','Injury') DEFAULT NULL,
+  `status_of_disability` enum('Permanent','Temporary') NOT NULL,
+  `houseno_street_subdivision_address` varchar(200) DEFAULT NULL,
+  `barangay` enum('Aplaya','Balibago','Caingin','Dila','Dita','Don Jose','Ibaba','Kanluran','Labas','Macabling','Malitlit','Malusak','Market Area','Pook','Pulong Santa Cruz','Santo Domingo','Sinalhan','Tagapo') NOT NULL,
+  `city_municipality` varchar(128) NOT NULL DEFAULT 'CITY OF SANTA ROSA',
+  `province` varchar(128) NOT NULL DEFAULT 'LAGUNA',
+  `region` varchar(128) NOT NULL DEFAULT 'IV-A (CALABARZON)',
+  `landline` varchar(15) DEFAULT NULL,
+  `mobile_number` varchar(15) DEFAULT NULL,
   `email` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL
+  `date_of_birth` date NOT NULL,
+  `sex` enum('Male','Female') NOT NULL,
+  `religion` varchar(128) NOT NULL,
+  `civil_status` enum('Single','Separated','Cohabitation','Married','Widow') NOT NULL,
+  `educational_attainment` enum('None','Kindergarten','Elementary','Junior High School','Senior High School','College','Vocational','Post Graduate Program') NOT NULL,
+  `is_voter` enum('Yes','No','17 Below') NOT NULL,
+  `employment_status` enum('Employed','Unemployed','Self-employed','Student') NOT NULL,
+  `income` enum('Less than 10,000','10,000 - 20, 000','21,000 - 40,000','41,000 - 100,000','More than 100,000') DEFAULT NULL,
+  `category_of_employment` enum('Government','Private') DEFAULT NULL,
+  `nature_of_employment` enum('Permanent/Regular','Emergency','Seasonal','Casual') DEFAULT NULL,
+  `occupation` enum('Managers','Professionals','Technician & Associate Professionals','Clerical Support Workers','Service & Sales Workers','Skilled Agricultural, Forestry & Fishery Workers','Craft & Related Trade Workers','Plant & Machine Operators & Assemblers','Elementary Occupations') DEFAULT NULL,
+  `other_occupation` varchar(128) DEFAULT NULL,
+  `is_4ps_beneficiary` enum('Yes','No') NOT NULL,
+  `blood_type` enum('A+','A-','B+','B-','AB+','AB-','O+','O-') DEFAULT NULL,
+  `organization_affiliated` varchar(128) DEFAULT NULL,
+  `contact_person` varchar(128) DEFAULT NULL,
+  `office_address` varchar(128) DEFAULT NULL,
+  `office_telephone_number` varchar(128) DEFAULT NULL,
+  `sss_number` varchar(128) DEFAULT NULL,
+  `gsis_number` varchar(128) DEFAULT NULL,
+  `psn_number` varchar(128) DEFAULT NULL,
+  `philhealth_number` varchar(128) DEFAULT NULL,
+  `philhealth_member_type` enum('PhilHealth Member',' PhilHealth Member-Dependent') DEFAULT NULL,
+  `father_last_name` varchar(128) DEFAULT NULL,
+  `father_first_name` varchar(128) DEFAULT NULL,
+  `father_middle_name` varchar(128) DEFAULT NULL,
+  `mother_last_name` varchar(128) DEFAULT NULL,
+  `mother_first_name` varchar(128) DEFAULT NULL,
+  `mother_middle_name` varchar(128) DEFAULT NULL,
+  `guardian_last_name` varchar(128) DEFAULT NULL,
+  `guardian_first_name` varchar(128) DEFAULT NULL,
+  `guardian_middle_name` varchar(128) DEFAULT NULL,
+  `guardian_relationship` varchar(128) DEFAULT NULL,
+  `guardian_contact_number` varchar(128) DEFAULT NULL,
+  `accomplished_by` enum('Applicant','Guardian','Representative') NOT NULL,
+  `name_of_accomplisher` varchar(128) NOT NULL,
+  `name_of_physician` varchar(128) NOT NULL,
+  `license_number` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -75,25 +99,8 @@ CREATE TABLE `user` (
 -- Indexes for table `pwd_data`
 --
 ALTER TABLE `pwd_data`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `senior_citizen_data`
---
-ALTER TABLE `senior_citizen_data`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `solo_parent_data`
---
-ALTER TABLE `solo_parent_data`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -103,24 +110,6 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `pwd_data`
 --
 ALTER TABLE `pwd_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `senior_citizen_data`
---
-ALTER TABLE `senior_citizen_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `solo_parent_data`
---
-ALTER TABLE `solo_parent_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
