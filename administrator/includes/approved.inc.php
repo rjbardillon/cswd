7f5dd0d538ccfef875afcb8411290a802e97f459
Romeo Bardillon <rmbardillonjr@iskolarngbayan.pup.edu.ph>
2022-08-24 09:49:20 +0800

    Update Administrator

--- administrator/includes/approved.inc.php
+++ administrator/includes/approved.inc.php
@@ -4,17 +4,23 @@ if (isset($_POST['pwd-approved'])) {
     require_once'dbh.inc.php';
     session_start();
     $id = $_SESSION['id'];
+    $currentMonth = date("m");
+    $currentDay = date("d");
+    $currentYear = date("Y");
+    $currentYear = (int)$currentYear + 3;
+    $expirationDate = $currentYear."-".$currentMonth."-".$currentDay;
 
-    $sql = "UPDATE pwd_data SET status = 'Approved' WHERE id = ?;";
+    $sql = "UPDATE pwd_data SET status = 'Approved', expiration_date = ? WHERE id = ?;";
     $stmt = $connection->prepare($sql);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../profile.html?error=stmterror");
         exit();
     }
 
-    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_bind_param($stmt, "ss", $expirationDate, $id);
     mysqli_stmt_execute($stmt); 
     mysqli_stmt_close($stmt);
+    // echo($expirationDate);
     header("location: ../pending-requests.html?error=none");
     exit();
 } else if (isset($_POST['pwd-reject'])) {
@@ -38,15 +44,20 @@ if (isset($_POST['pwd-approved'])) {
     require_once'dbh.inc.php';
     session_start();
     $id = $_SESSION['id'];
+    $currentMonth = date("m");
+    $currentDay = date("d");
+    $currentYear = date("Y");
+    $currentYear = (int)$currentYear + 3;
+    $expirationDate = $currentYear."-".$currentMonth."-".$currentDay;
 
-    $sql = "UPDATE senior_citizen_data SET status = 'Approved' WHERE id = ?;";
+    $sql = "UPDATE senior_citizen_data SET status = 'Approved', expiration_date = ? WHERE id = ?;";
     $stmt = $connection->prepare($sql);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../profile.html?error=stmterror");
         exit();
     }
 
-    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_bind_param($stmt, "ss", $expirationDate, $id);
     mysqli_stmt_execute($stmt); 
     mysqli_stmt_close($stmt);
     header("location: ../pending-requests.html?error=none");
@@ -72,15 +83,20 @@ if (isset($_POST['pwd-approved'])) {
     require_once'dbh.inc.php';
     session_start();
     $id = $_SESSION['id'];
+    $currentMonth = date("m");
+    $currentDay = date("d");
+    $currentYear = date("Y");
+    $currentYear = (int)$currentYear + 3;
+    $expirationDate = $currentYear."-".$currentMonth."-".$currentDay;
 
-    $sql = "UPDATE solo_parent_data SET status = 'Approved' WHERE id = ?;";
+    $sql = "UPDATE solo_parent_data SET status = 'Approved', expiration_date = ? WHERE id = ?;";
     $stmt = $connection->prepare($sql);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../profile.html?error=stmterror");
         exit();
     }
 
-    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_bind_param($stmt, "ss", $expirationDate, $id);
     mysqli_stmt_execute($stmt); 
     mysqli_stmt_close($stmt);
     header("location: ../pending-requests.html?error=none");
