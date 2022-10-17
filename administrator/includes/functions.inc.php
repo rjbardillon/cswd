2c205f044b26b94817cd2b450e8f1660907d0d08
Romeo Bardillon <rmbardillonjr@iskolarngbayan.pup.edu.ph>
2022-10-17 20:01:00 +0800

    Added reset password and PHPMailer library

--- administrator/includes/functions.inc.php
+++ administrator/includes/functions.inc.php
@@ -1,5 +1,32 @@
 <?php
 
+function email($senderName, $subject, $senderEmail, $receiverName, $receiverEmail, $message) {
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
+    $mail = new PHPMailer(true);
+    // $mail->SMTPDebug = 3;
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "populationmanagementsystem@gmail.com";
+    $mail->Password = "ntllidrufjiuujll";
+    $mail->SMTPSecure = "tls";
+    $mail->Port = "587";
+    $mail->From = $senderEmail;
+    $mail->FromName = $senderName;
+    $mail->addAddress($receiverEmail, $receiverName);
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->AltBody = "This is the plain text version of email content";
+    if (!$mail->send()) {
+        echo "Email failed";
+        exit();
+    }
+}
+
 function ageCalculator($dob){
     if(!empty($dob)){
         $birthdate = new DateTime($dob);
