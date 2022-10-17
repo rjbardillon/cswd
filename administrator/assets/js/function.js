cfba2e78b168dd65bbdbdb35319e7c15aae49f5a
Romeo Bardillon <rmbardillonjr@iskolarngbayan.pup.edu.ph>
2022-10-17 15:04:30 +0800

    Added table sorting and limit showing of data

--- administrator/assets/js/function.js
+++ administrator/assets/js/function.js
@@ -1,29 +1,41 @@
+$(document).ready( function () {
+    $('#mainAdministrators').DataTable();
+    $('#pwdAdministrators').DataTable();
+    $('#soloparentAdministrators').DataTable();
+    $('#srCitizenAdministrators').DataTable();
+    $('#pwdDataTable').DataTable();
+    $('#spDataTable').DataTable();
+    $('#scDataTable').DataTable();
+});
+
 // Profile Picture
 const imgDiv = document.querySelector('.profile-pic-div');
-const img = document.querySelector('.photo');
-const file = document.querySelector('#file');
-const uploadBtn = document.querySelector('#uploadBtn');
+if (imgDiv !== null) {
+    const img = document.querySelector('.photo');
+    const file = document.querySelector('#file');
+    const uploadBtn = document.querySelector('#uploadBtn');
 
 
-imgDiv.addEventListener('mouseenter', function(){
-    uploadBtn.style.display = "block";
-});
+    imgDiv.addEventListener('mouseenter', function(){
+        uploadBtn.style.display = "block";
+    });
 
-imgDiv.addEventListener('mouseleave', function(){
-    uploadBtn.style.display = "none";
-});
+    imgDiv.addEventListener('mouseleave', function(){
+        uploadBtn.style.display = "none";
+    });
 
-file.addEventListener('change', function(){
-    const choosedFile = this.files[0];
+    file.addEventListener('change', function(){
+        const choosedFile = this.files[0];
 
-    if (choosedFile) {
+        if (choosedFile) {
 
-        const reader = new FileReader();
+            const reader = new FileReader();
 
-        reader.addEventListener('load', function(){
-            img.setAttribute('src', reader.result);
-        });
+            reader.addEventListener('load', function(){
+                img.setAttribute('src', reader.result);
+            });
 
-        reader.readAsDataURL(choosedFile);
-    }
-});
+            reader.readAsDataURL(choosedFile);
+        }
+    });
+}
